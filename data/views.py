1a133cb619a3e4f4af083f8c4b52b62fca79ed20
Ben <benwiessner.dev@gmail.com>
2023-05-31 22:57:03 +0200

    integration einer base

--- data/views.py
+++ data/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def dashboard(request):
-    return render(request, 'data/dashboard.html', {})
+def home(request):
+    return render(request, 'data/home.html', {})
