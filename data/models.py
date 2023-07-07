45c8b1df0921c3cacdf29195c934882bfdbaba56
Ben <benwiessner.dev@gmail.com>
2023-07-07 15:56:52 +0200

    new table names

--- data/models.py
+++ data/models.py
@@ -6,6 +6,9 @@ class Femurkomponente(models.Model):
     material = models.CharField('Material')
     groeße = models.FloatField('Größe')         # in cm / mm
 
+    class Meta:
+        verbose_name_plural = "Femurkomponenten"
+
 class Tibiaplateau(models.Model):
     hersteller = models.CharField('Hersteller')
     modell = models.CharField('Modell')
@@ -18,11 +21,17 @@ class Patellaersatz(models.Model):
     material = models.CharField('Material')
     groeße = models.FloatField('Größe')         # in cm / mm
 
+    class Meta:
+        verbose_name_plural = "Patellaersätze"
+
 class Kopf(models.Model):
     hersteller = models.CharField('Hersteller')
     modell = models.CharField('Modell')
     material = models.CharField('Material')
     groeße = models.FloatField('Größe')         # in cm / mm
+
+    class Meta:
+        verbose_name_plural = "Köpfe"
 
 class Inlay(models.Model):
     hersteller = models.CharField('Hersteller')
@@ -36,23 +45,38 @@ class Schaft(models.Model):
     material = models.CharField('Material')
     groeße = models.FloatField('Größe')         # in cm / mm
 
+    class Meta:
+        verbose_name_plural = "Schafte"
+
 class Pfanne(models.Model):
     hersteller = models.CharField('Hersteller')
     modell = models.CharField('Modell')
     material = models.CharField('Material')
     groeße = models.FloatField('Größe')         # in cm / mm
 
+    class Meta:
+        verbose_name_plural = "Pfannen"
+
 class Reoperation(models.Model):
     reoperation = models.BooleanField('Reoperation')
     reoperation_datum = models.DateTimeField('Reoperationsdatum')
+
+    class Meta:
+        verbose_name_plural = "Reperationen"
 
 class Patient(models.Model):
     geburtsdatum = models.DateField('Geburtsdatum')
     gewicht = models.FloatField('Gewicht')          # in kg
 
+    class Meta:
+        verbose_name_plural = "Patienten"
+
 class Lagerort(models.Model):
     schrank = models.IntegerField('Schrank')
     kiste = models.IntegerField('Kiste')
+
+    class Meta:
+        verbose_name_plural = "Lagerorte"
 
 class Explantat(models.Model):
     ursache = models.TextField('Ursache')
@@ -66,7 +90,7 @@ class Explantat(models.Model):
     bild = models.FileField('Bild', blank=True, null=True)
     lagerort = models.ForeignKey(Lagerort, blank=True, null=True, on_delete=models.CASCADE)         
     patient = models.ForeignKey(Patient, blank=True, null=True, on_delete=models.CASCADE)           
-    reoperation = models.ManyToManyField(Reoperation, blank=True, null=True)   # 1:n
+    reoperation = models.ManyToManyField(Reoperation, blank=True)   # 1:n
     inlay = models.ForeignKey(Inlay, on_delete=models.CASCADE)
     
     # Hüftexplantate
@@ -78,3 +102,6 @@ class Explantat(models.Model):
     femurkomponente = models.ForeignKey(Femurkomponente, blank=True, null=True, on_delete=models.CASCADE)
     tibiaplateau = models.ForeignKey(Tibiaplateau, blank=True, null=True, on_delete=models.CASCADE)
     patellaersatz = models.ForeignKey(Patellaersatz, blank=True, null=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Explantate"
