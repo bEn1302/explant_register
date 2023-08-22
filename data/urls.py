159eb6a95ff97537efac2d91d2adc6f655e5e316
Ben <benwiessner.dev@gmail.com>
2023-08-22 22:00:15 +0200

    admin stuff

--- data/urls.py
+++ data/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('analytics', views.all_analytics, name="analytic-explants"),
     path('explants', views.explants_table_view, name="table-explants"),
