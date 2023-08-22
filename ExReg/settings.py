36fa3bb548ec2b73b9da13bd7d7cdf575b4b21d1
Ben <benwiessner.dev@gmail.com>
2023-08-22 21:05:38 +0200

    update v9

--- ExReg/settings.py
+++ ExReg/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
 
 # STATICFILES_DIRS = ( 
 #       '/Users/benwiessner/Documents/Programmieren/WebDev/Django/explant_register/static/',  
