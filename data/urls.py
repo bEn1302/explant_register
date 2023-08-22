c16d4d2215b917b4afd785212aaf3bd0097d5755
Ben <benwiessner.dev@gmail.com>
2023-08-22 16:18:29 +0200

    Table

--- data/urls.py
+++ data/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('analytics', views.all_analytics, name="analytic-explants"),
+    path('explants', views.all_explants, name="table-explants"),
 ]
--- data/views.py
+++ data/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+# Pull Data from Datatable:
+from .models import Explantat
+
 # Create your views here.
+
+def all_analytics(request):
+    return render(request, 'data/explant_analytic.html')
+
+def all_explants(request):
+    explant_table = Explantat.objects.all()
+    return render(request, 'data/explant_table.html',{'explant_table': explant_table})
+
 def home(request):
     return render(request, 'data/home.html')
