58a1db230b31742aa72037cc860f8928fb6094d2
Ben <benwiessner.dev@gmail.com>
2023-10-05 18:33:44 +0200

    Anmeldung

--- users/views.py
+++ users/views.py
@@ -3,4 +3,17 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def login_user(request):
-    return render(request, 'authentication/sign_in.html', {})
+    if request.method == 'POST':
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+            ...
+        else:
+            messages.success(request, ("Incorrect username or password."))
+            return redirect('sign_in')
+    else:
+        return render(request, 'registration/sign_in.html', {})
