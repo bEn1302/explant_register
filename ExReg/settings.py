be79036157f7284305e988f59c26fb2db348c04b
Ben <benwiessner.dev@gmail.com>
2023-10-07 14:14:02 +0200

    Test

--- ExReg/settings.py
+++ ExReg/settings.py
@@ -1,13 +1,11 @@
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
