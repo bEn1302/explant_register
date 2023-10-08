6a9ad535e804bf54ccb4e402320fe92cc0cb928b
Ben <benwiessner.dev@gmail.com>
2023-10-08 21:45:03 +0200

    Änderungen bas.html & js

---
+++ staticfiles/js/expand_table.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const triggerCells = document.querySelectorAll(".trigger-cell");
+
+    triggerCells.forEach((cell) => {
+      // Create an icon element and add it to the cell
+      const icon = document.createElement("i");
+      icon.classList.add("bi", "bi-plus-circle");
+      cell.appendChild(icon);
+
+      cell.addEventListener("click", function () {
+        const extendedRow = this.closest(".table-row").nextElementSibling;
+
+        // Toggle the visibility of the extended row
+        extendedRow.classList.toggle("d-none");
+
+        // Toggle the class of the icon to change its appearance
+        icon.classList.toggle("bi-plus-circle");
+        icon.classList.toggle("bi-dash-circle");
+      });
+    });
+  });
---
+++ staticfiles/js/update_form.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    // AJAX-Anfrage beim Klicken auf den "Speichern"-Button im Modal
+    $('.update-form').submit(function(e) {
+        e.preventDefault(); // Verhindert das Standardverhalten des Formulars
+
+        var form = $(this);
+        
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'), // Verwenden Sie den action-Wert aus dem Formular
+            data: form.serialize(),
+            success: function(data) {
+                if (data.success) {
+                    // Erfolgreiche Aktualisierung, zeige Erfolgsmeldung im Modal
+                    $('#alert-container').html('<div class="alert alert-success" role="alert">Erfolgreich aktualisiert.</div>');
+                } else {
+                    // Fehler beim Formular, zeige Fehlermeldung im Modal
+                    $('#alert-container').html('<div class="alert alert-danger" role="alert">Fehler beim Aktualisieren.</div>');
+                }
+            }
+        });
+    });
+});
