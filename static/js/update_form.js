71406dd1c5076f4c3bb8e1ac093624e59d5d024a
Ben <benwiessner.dev@gmail.com>
2023-10-08 20:26:19 +0200

    Änderung update_form.js

--- static/js/update_form.js
+++ static/js/update_form.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // AJAX-Anfrage beim Klicken auf den "Speichern"-Button im Modal
-    $('#lagerortUpdateForm').submit(function(e) {
+    $('.update-form').submit(function(e) {
         e.preventDefault(); // Verhindert das Standardverhalten des Formulars
 
         var form = $(this);
@@ -12,10 +12,10 @@ $(document).ready(function() {
             success: function(data) {
                 if (data.success) {
                     // Erfolgreiche Aktualisierung, zeige Erfolgsmeldung im Modal
-                    $('#alert-container').html('<div class="alert alert-success" role="alert">Lagerort erfolgreich aktualisiert.</div>');
+                    $('#alert-container').html('<div class="alert alert-success" role="alert">Erfolgreich aktualisiert.</div>');
                 } else {
                     // Fehler beim Formular, zeige Fehlermeldung im Modal
-                    $('#alert-container').html('<div class="alert alert-danger" role="alert">Fehler beim Aktualisieren des Lagerorts.</div>');
+                    $('#alert-container').html('<div class="alert alert-danger" role="alert">Fehler beim Aktualisieren.</div>');
                 }
             }
         });
