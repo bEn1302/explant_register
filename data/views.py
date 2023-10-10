8a8259d86465201e7647ea1159213fb90b719c1e
Ben <benwiessner.dev@gmail.com>
2023-10-10 10:11:18 +0200

    Forms.py Rückgängig gemacht

--- data/views.py
+++ data/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseRedirect
-
 from .forms import *
 
 def start(request):
