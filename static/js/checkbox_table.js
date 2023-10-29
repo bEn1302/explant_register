a4dcbef323831b44fc04134a169b7b96195dd0c9
Ben <benwiessner.dev@gmail.com>
2023-10-29 11:38:34 +0100

    Checkbox fix

--- static/js/checkbox_table.js
+++ static/js/checkbox_table.js
@@ -1,24 +1,6 @@
-// Funktion, um alle Checkboxen zu aktivieren/deaktivieren
-function toggleCheckboxes(checked) {
+document.getElementById('selectAll').addEventListener('change', function() {
     var checkboxes = document.querySelectorAll('.row-checkbox');
-    checkboxes.forEach(function (checkbox) {
-    checkbox.checked = checked;
-    });
-}
-
-// Hör auf Änderungen an der "Select All"-Checkbox
-document.getElementById('selectAll').addEventListener('change', function () {
-    toggleCheckboxes(this.checked);
-});
-
-// Hör auf Klick-Ereignisse auf den Tabelleneinträgen
-var rowCheckboxes = document.querySelectorAll('.row-checkbox');
-rowCheckboxes.forEach(function (checkbox) {
-    checkbox.addEventListener('change', function () {
-    // Hier kannst du die gewünschte Aktion ausführen, z.B. wenn eine Checkbox ausgewählt wird
-    if (this.checked) {
-        // Führe deine Aktion aus
-        console.log('Checkbox ausgewählt: ' + this.parentNode.parentNode.innerText);
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = this.checked;
     }
-    });
 });
