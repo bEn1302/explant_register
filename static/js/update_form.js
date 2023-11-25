68bd61cf9fba132d88af8017c1f0611ec90f1e06
Ben <benwiessner.dev@gmail.com>
2023-11-25 11:28:01 +0100

    change Design fix ?

--- static/js/update_form.js
+++ static/js/update_form.js
@@ -1,36 +1,13 @@
-// $(document).ready(function() {
-//     // AJAX-Anfrage beim Klicken auf den "Speichern"-Button im Modal
-//     $('#update-form{{ explant.id }}').submit(function(e) {
-//         e.preventDefault(); // Verhindert das Standardverhalten des Formulars
-
-//         var form = $(this);
-        
-//         $.ajax({
-//             type: 'POST',
-//             url: form.attr('action'), // Verwenden Sie den action-Wert aus dem Formular
-//             data: form.serialize(),
-//             success: function(data) {
-//                 if (data.success) {
-//                     // Erfolgreiche Aktualisierung, zeige Erfolgsmeldung im Modal
-//                     $('#alert-container').html('<div class="alert alert-success" role="alert">Lagerort erfolgreich aktualisiert.</div>');
-//                 } else {
-//                     // Fehler beim Formular, zeige Fehlermeldung im Modal
-//                     $('#alert-container').html('<div class="alert alert-danger" role="alert">Fehler beim Aktualisieren des Lagerorts.</div>');
-//                 }
-//             }
-//         });
-//     });
-// });
 $(document).ready(function() {
     // AJAX-Anfrage beim Klicken auf den "Speichern"-Button im Modal
     $('#update-form{{ explant.id }}').submit(function(e) {
         e.preventDefault(); // Verhindert das Standardverhalten des Formulars
 
         var form = $(this);
-
+        
         $.ajax({
-            type: form.attr('method'), // Use the method value from the form
-            url: form.attr('action'), // Use the action value from the form
+            type: 'POST',
+            url: form.attr('action'), // Verwenden Sie den action-Wert aus dem Formular
             data: form.serialize(),
             success: function(data) {
                 if (data.success) {
@@ -40,10 +17,6 @@ $(document).ready(function() {
                     // Fehler beim Formular, zeige Fehlermeldung im Modal
                     $('#alert-container').html('<div class="alert alert-danger" role="alert">Fehler beim Aktualisieren des Lagerorts.</div>');
                 }
-            },
-            error: function(xhr, status, error) {
-                console.error('AJAX request failed:', status, error);
-                // Handle the error, show an error message if needed
             }
         });
     });
