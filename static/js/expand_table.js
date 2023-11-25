8d9c6e1c7347062ecc2cdff17b22684a8c4d89a8
Ben <benwiessner.dev@gmail.com>
2023-11-25 11:20:29 +0100

    Test new Table Design

--- static/js/expand_table.js
+++ static/js/expand_table.js
@@ -1,21 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const triggerCells = document.querySelectorAll(".trigger-cell");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const triggerCells = document.querySelectorAll(".trigger-cell");
 
-    triggerCells.forEach((cell) => {
+//     triggerCells.forEach((cell) => {
+//       // Create an icon element and add it to the cell
+//       const icon = document.createElement("i");
+//       icon.classList.add("bi", "bi-plus-circle");
+//       cell.appendChild(icon);
+
+//       cell.addEventListener("click", function () {
+//         const extendedRow = this.closest(".table-row").nextElementSibling;
+
+//         // Toggle the visibility of the extended row
+//         extendedRow.classList.toggle("d-none");
+
+//         // Toggle the class of the icon to change its appearance
+//         icon.classList.toggle("bi-plus-circle");
+//         icon.classList.toggle("bi-dash-circle");
+//       });
+//     });
+//   });
+document.addEventListener("DOMContentLoaded", function () {
+  const triggerCells = document.querySelectorAll(".trigger-cell");
+
+  triggerCells.forEach((cell) => {
       // Create an icon element and add it to the cell
       const icon = document.createElement("i");
       icon.classList.add("bi", "bi-plus-circle");
       cell.appendChild(icon);
 
       cell.addEventListener("click", function () {
-        const extendedRow = this.closest(".table-row").nextElementSibling;
+          const extendedRow = this.closest(".table-row").nextElementSibling;
 
-        // Toggle the visibility of the extended row
-        extendedRow.classList.toggle("d-none");
+          // Toggle the visibility of the extended row
+          if (extendedRow.style.display === "table-row") {
+              extendedRow.style.display = "none";
+          } else {
+              extendedRow.style.display = "table-row";
+          }
 
-        // Toggle the class of the icon to change its appearance
-        icon.classList.toggle("bi-plus-circle");
-        icon.classList.toggle("bi-dash-circle");
+          // Toggle the class of the icon to change its appearance
+          icon.classList.toggle("bi-plus-circle");
+          icon.classList.toggle("bi-dash-circle");
       });
-    });
   });
+});
--- static/js/update_form.js
+++ static/js/update_form.js
@@ -1,13 +1,36 @@
+// $(document).ready(function() {
+//     // AJAX-Anfrage beim Klicken auf den "Speichern"-Button im Modal
+//     $('#update-form{{ explant.id }}').submit(function(e) {
+//         e.preventDefault(); // Verhindert das Standardverhalten des Formulars
+
+//         var form = $(this);
+        
+//         $.ajax({
+//             type: 'POST',
+//             url: form.attr('action'), // Verwenden Sie den action-Wert aus dem Formular
+//             data: form.serialize(),
+//             success: function(data) {
+//                 if (data.success) {
+//                     // Erfolgreiche Aktualisierung, zeige Erfolgsmeldung im Modal
+//                     $('#alert-container').html('<div class="alert alert-success" role="alert">Lagerort erfolgreich aktualisiert.</div>');
+//                 } else {
+//                     // Fehler beim Formular, zeige Fehlermeldung im Modal
+//                     $('#alert-container').html('<div class="alert alert-danger" role="alert">Fehler beim Aktualisieren des Lagerorts.</div>');
+//                 }
+//             }
+//         });
+//     });
+// });
 $(document).ready(function() {
     // AJAX-Anfrage beim Klicken auf den "Speichern"-Button im Modal
     $('#update-form{{ explant.id }}').submit(function(e) {
         e.preventDefault(); // Verhindert das Standardverhalten des Formulars
 
         var form = $(this);
-        
+
         $.ajax({
-            type: 'POST',
-            url: form.attr('action'), // Verwenden Sie den action-Wert aus dem Formular
+            type: form.attr('method'), // Use the method value from the form
+            url: form.attr('action'), // Use the action value from the form
             data: form.serialize(),
             success: function(data) {
                 if (data.success) {
@@ -17,6 +40,10 @@ $(document).ready(function() {
                     // Fehler beim Formular, zeige Fehlermeldung im Modal
                     $('#alert-container').html('<div class="alert alert-danger" role="alert">Fehler beim Aktualisieren des Lagerorts.</div>');
                 }
+            },
+            error: function(xhr, status, error) {
+                console.error('AJAX request failed:', status, error);
+                // Handle the error, show an error message if needed
             }
         });
     });
