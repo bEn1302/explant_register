dbc489d9cf351723d99e10f7aac855c75a1ab89d
Ben <benwiessner.dev@gmail.com>
2023-11-28 07:19:51 +0100

    Delete Function v3

--- static/js/delete_function.js
+++ static/js/delete_function.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     var deleteButtons = document.querySelectorAll('.deleteButton');
-    var deleteForms = document.querySelectorAll('.deleteForm');
-
-    deleteButtons.forEach(function(deleteButton, index) {
+    
+    deleteButtons.forEach(function(deleteButton) {
         deleteButton.addEventListener('click', function() {
-            var form = deleteForms[index];
+            var form = deleteButton.closest('form');
             if (confirm("Are you sure you want to delete the selected explantate(s)?")) {
                 form.submit();
             }
