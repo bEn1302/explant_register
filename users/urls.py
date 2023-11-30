2b56d4fbf630660b55be693173adac7b4d6f706c
Ben <benwiessner.dev@gmail.com>
2023-11-30 17:42:35 +0100

    Update href for not authenticated users

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sign_in', views.login_user, name='sign_in'),
-    path('sign_out', views.logout_user, name='sign_out'),
-    path('sign_up', views.register_user, name='sign_up'),
+    path('sign_in/', views.login_user, name='sign_in'),
+    path('sign_out/', views.logout_user, name='sign_out'),
+    path('sign_up/', views.register_user, name='sign_up'),
 ]
