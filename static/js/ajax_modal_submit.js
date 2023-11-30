7f2d6e3a96fd790a6dc5e09a3f5296707a415230
Ben <benwiessner.dev@gmail.com>
2023-11-30 16:50:32 +0100

    Test 3 ajax

--- static/js/ajax_modal_submit.js
+++ static/js/ajax_modal_submit.js
@@ -1,64 +1,28 @@
-function handleFormSubmit(form, modalId, successCallback) {
-    $.ajax({
-        type: form.attr('method'),
-        url: form.attr('action'),
-        data: form.serialize(),
-        success: function (data) {
-            if (data.success) {
-                $('#' + modalId).modal('hide');
-                if (successCallback) {
-                    successCallback(data);
-                }
-            } else {
-                $('#alert-container').html('<div class="alert alert-danger">' + data.errors + '</div>');
-            }
-        },
-        error: function () {
-            alert('Ein Fehler ist aufgetreten. Bitte versuche es erneut.');
-        }
-    });
-}
-
-function saveFormDataToDB(form) {
-    $.ajax({
-        type: form.attr('method'),
-        url: form.attr('action'),
-        data: form.serialize(),
-        success: function (data) {
-            if (data.success) {
-                console.log('Daten erfolgreich in der Datenbank gespeichert.');
-            } else {
-                console.log('Fehler beim Speichern in der Datenbank:', data.errors);
-            }
-        },
-        error: function () {
-            console.log('AJAX-Anfragefehler beim Speichern in der Datenbank.');
-        }
-    });
-}
-
 $(document).ready(function () {
-    // Generische Funktion zum Hinzufügen von Modal-Formularen
-    function addModalForm(formId, modalId, submitButtonId, successCallback) {
-        $('#' + submitButtonId).click(function () {
-            var form = $('#' + formId);
-            handleFormSubmit(form, modalId, successCallback);
-            saveFormDataToDB(form);
+    $('.add-button').click(function (e) {
+        e.preventDefault();
+
+        var formId = $(this).data('form-id');
+        var form = $('#' + formId);
+
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: form.serialize(),
+            success: function (data) {
+                if (data.success) {
+                    // Erfolgreiche Verarbeitung: Hier kannst du das Formular aktualisieren oder eine Benachrichtigung anzeigen.
+                    alert('Daten erfolgreich hinzugefügt!');
+                    // Beispiel: Formular zurücksetzen
+                    form.trigger('reset');
+                } else {
+                    // Fehler beim Speichern: Hier kannst du Fehler anzeigen oder andere Maßnahmen ergreifen.
+                    alert('Fehler beim Hinzufügen der Daten:\n' + JSON.stringify(data.errors));
+                }
+            },
+            error: function () {
+                alert('Fehler bei der AJAX-Anfrage');
+            }
         });
-    }
-
-    // Beispiel für ein Modal-Formular
-    addModalForm('addInlayForm', 'addInlayModal', 'addInlayButton', function (data) {
-        // Hier kannst du nach einem erfolgreichen Hinzufügen Anpassungen vornehmen
-        $('#id_inlay').val('');
     });
-
-    // Füge ähnliche Event-Listener für andere Modal-Formulare hinzu
-    addModalForm('addKopfForm', 'addKopfModal', 'addKopfButton', function (data) {
-        // Weitere Anpassungen für das Kopf-Modal
-        $('#id_kopf').val('');
-    });
-
-    // Wiederhole dies für alle Modal-Formulare
-    // ...
 });
