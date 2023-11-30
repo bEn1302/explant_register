1a4d5812ca5bfd203a51af03569419fd9d1c4543
Ben <benwiessner.dev@gmail.com>
2023-11-30 16:27:04 +0100

    Test forms 2

--- static/js/ajax_modal_submit.js
+++ static/js/ajax_modal_submit.js
@@ -1,43 +1,64 @@
-function handleAjaxFormSubmit(form, modalId, successCallback) {
+function handleFormSubmit(form, modalId, successCallback) {
     $.ajax({
         type: form.attr('method'),
-        url: form.data('url'),
+        url: form.attr('action'),
         data: form.serialize(),
         success: function (data) {
             if (data.success) {
-                // Erfolgreich hinzugefügt - schließe das Modal oder mach hier etwas nach Bedarf
                 $('#' + modalId).modal('hide');
                 if (successCallback) {
                     successCallback(data);
                 }
             } else {
-                // Fehler beim Hinzufügen - zeige Fehlermeldungen an
                 $('#alert-container').html('<div class="alert alert-danger">' + data.errors + '</div>');
             }
         },
         error: function () {
-            // AJAX-Anfragefehler
             alert('Ein Fehler ist aufgetreten. Bitte versuche es erneut.');
         }
     });
 }
 
+function saveFormDataToDB(form) {
+    $.ajax({
+        type: form.attr('method'),
+        url: form.attr('action'),
+        data: form.serialize(),
+        success: function (data) {
+            if (data.success) {
+                console.log('Daten erfolgreich in der Datenbank gespeichert.');
+            } else {
+                console.log('Fehler beim Speichern in der Datenbank:', data.errors);
+            }
+        },
+        error: function () {
+            console.log('AJAX-Anfragefehler beim Speichern in der Datenbank.');
+        }
+    });
+}
+
 $(document).ready(function () {
-    $('#addLagerortButton').click(function () {
-        var form = $('#addLagerortForm');
-        handleAjaxFormSubmit(form, 'addLagerortModal', function (data) {
-            // Beispiel: Leere das Formularfeld für Lagerort
-            $('#id_lagerort').val('');
-            // Hier kannst du weitere Anpassungen nach einem erfolgreichen Hinzufügen vornehmen
+    // Generische Funktion zum Hinzufügen von Modal-Formularen
+    function addModalForm(formId, modalId, submitButtonId, successCallback) {
+        $('#' + submitButtonId).click(function () {
+            var form = $('#' + formId);
+            handleFormSubmit(form, modalId, successCallback);
+            saveFormDataToDB(form);
         });
+    }
+
+    // Beispiel für ein Modal-Formular
+    addModalForm('addInlayForm', 'addInlayModal', 'addInlayButton', function (data) {
+        // Hier kannst du nach einem erfolgreichen Hinzufügen Anpassungen vornehmen
+        $('#id_inlay').val('');
     });
 
-    // Füge ähnliche Event-Listener für andere Modals hinzu
-    // Beispiel:
-    // $('#addPatientButton').click(function () {
-    //     var form = $('#addPatientForm');
-    //     handleAjaxFormSubmit(form, 'addPatientModal', function (data) {
-    //         // Weitere Anpassungen für das Patienten-Modal
-    //     });
-    // });
+    // Füge ähnliche Event-Listener für andere Modal-Formulare hinzu
+    addModalForm('addKopfForm', 'addKopfModal', 'addKopfButton', function (data) {
+        // Weitere Anpassungen für das Kopf-Modal
+        $('#id_kopf').val('');
+    });
+
+    // Wiederhole dies für alle Modal-Formulare
+    // ...
 });
