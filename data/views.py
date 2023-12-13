89bfb660959a7e63b7c2d12527c1895ba818bb27
Ben <benwiessner.dev@gmail.com>
2023-12-13 18:35:12 +0100

    views fix text-decoration removed

--- data/views.py
+++ data/views.py
@@ -99,13 +99,13 @@ def all_analytics(request):
 
 # --------------------------- Update Explants ---------------------------
 def explant_update(request, explant_id):
-    explant = Explantat.objects.get(pk=explant_id)
-    form = ExplantatForm(request.POST or None, instance=explant)
+    explantat_form = Explantat.objects.get(pk=explant_id)
+    form = ExplantatForm(request.POST or None, instance=explantat_form)
     if form.is_valid():
         form.save()
         return redirect('table-explants')
     
-    return render(request, 'data/update_explant.html', {'explant': explant, 'form':form})
+    return render(request, 'data/update_explant.html', {'explantat_form': explantat_form, 'form':form})
 
 
 # --------------------------- Data Insert ---------------------------
