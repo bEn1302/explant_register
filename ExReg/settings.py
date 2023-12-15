c15e62da8f6e3d4a328ff5e13f776442acb548fa
Ben <benwiessner.dev@gmail.com>
2023-12-15 17:17:00 +0100

    update max file size and new forms.py

--- ExReg/settings.py
+++ ExReg/settings.py
@@ -135,6 +135,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
+
 # STATICFILES_DIRS = ( 
 #       os.path.join(BASE_DIR, 'static'),
 
