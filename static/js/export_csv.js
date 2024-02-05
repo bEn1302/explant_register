0ff255712fc96339d071fd1107424f56034dd4eb
Ben <benwiessner.dev@gmail.com>
2024-02-05 14:07:22 +0100

    new try (4)

--- static/js/export_csv.js
+++ static/js/export_csv.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Handler für den Klick auf den Export-Button
+    // Handler for the click on the export button
     document.getElementById("exportButton").addEventListener("click", function(event) {
-        // Array zum Sammeln der ausgewählten IDs
+        event.preventDefault(); // Prevent the default behavior of the button
+        
+        // Array to collect the selected IDs
         var selectedIds = [];
         
-        // Überprüfen Sie jede Checkbox und sammeln Sie die ausgewählten IDs
-        document.querySelectorAll(".table-row input[type='checkbox']:checked").forEach(function(checkbox) {
+        // Check if the table contains class 'huefte' or 'knie' to determine the checkbox class
+        var className = document.querySelector('#explant-table').classList.contains('huefte') ? 'huefte-row-checkbox' : 'knie-row-checkbox';
+        
+        // Checkboxes to gather selected IDs
+        var checkboxes = document.querySelectorAll('.' + className + ':checked');
+        
+        // Collect IDs from selected checkboxes
+        checkboxes.forEach(function(checkbox) {
             selectedIds.push(checkbox.value);
         });
         
-        // Überprüfen, ob mindestens ein Datensatz ausgewählt wurde
+        // Check if at least one dataset is selected
         if (selectedIds.length > 0) {
-            // Erstellen Sie den URL für den CSV-Export und fügen Sie die ausgewählten IDs als Parameter hinzu
+            // Create the URL for the CSV export and add the selected IDs as parameters
             var exportUrl = "{% url 'explant_csv' %}?selected_ids=" + selectedIds.join("&selected_ids=");
             
-            // Weiterleitung zum CSV-Export-URL
+            // Redirect to the CSV export URL
             window.location.href = exportUrl;
         } else {
-            // Benutzer über fehlende Auswahl informieren
+            // Notify the user about the lack of selection
             alert("Bitte wählen Sie mindestens einen Datensatz aus.");
         }
     });
