9d6c91cde478a3748b95e4745c576e6a225bb362
Ben <benwiessner.dev@gmail.com>
2024-03-23 19:52:49 +0100

    User Profile DB stable

--- data/models.py
+++ data/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    profile_picture = models.ImageField(upload_to='images/', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
 
 class Femurkomponente(models.Model):
     hersteller = models.CharField('Hersteller')
