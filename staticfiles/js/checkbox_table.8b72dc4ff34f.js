b2267e3a35ac030cdf471529ffc440a20bd06a7b
Ben <benwiessner.dev@gmail.com>
2024-04-13 15:05:03 +0200

    refreshed staticfiles

--- staticfiles/js/checkbox_table.8b72dc4ff34f.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('selectAll').addEventListener('change', function() {
-    var className = document.querySelector('#explant-table').classList.contains('huefte') ? 'huefte-row-checkbox' : 'knie-row-checkbox';
-    var checkboxes = document.querySelectorAll('.' + className);
-    for (var i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].checked = this.checked;
-    }
-});
--- staticfiles/js/spinner copy.js
+++
@@ -1,9 +0,0 @@
-const spinnerWrapperE1 = document.querySelector('.spinner-wrapper')
-
-      window.addEventListener('load', () => {
-          spinnerWrapperE1.style.opacity = '0';
-
-          setTimeout(() => {
-            spinnerWrapperE1.style.display = 'none';
-          }, 200);
-      });
