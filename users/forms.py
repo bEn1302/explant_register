096d981bb48fd4da087f70c838df552e5da049d2
Ben <benwiessner.dev@gmail.com>
2024-05-27 14:45:09 +0200

    mehr Übersetzungen

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,13 @@
 from typing import Any
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 from django import forms
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(label="",help_text=False, widget=forms.EmailInput(attrs={'class':'form-control form-control-lg bg-light fs-6', 'placeholder':'Email address'}))
-    first_name = forms.CharField(label="",help_text=False, max_length=25, widget=forms.TextInput(attrs={'class':'form-control form-control-lg bg-light fs-6', 'placeholder':'First name'}))
-    last_name = forms.CharField(label="",help_text=False, max_length=25, widget=forms.TextInput(attrs={'class':'form-control form-control-lg bg-light fs-6', 'placeholder':'Last name'}))
+    email = forms.EmailField(label="",help_text=False, widget=forms.EmailInput(attrs={'class':'form-control form-control-lg bg-light fs-6', 'placeholder': _('Email-Adresse')}))
+    first_name = forms.CharField(label="",help_text=False, max_length=25, widget=forms.TextInput(attrs={'class':'form-control form-control-lg bg-light fs-6', 'placeholder':_('Vorname')}))
+    last_name = forms.CharField(label="",help_text=False, max_length=25, widget=forms.TextInput(attrs={'class':'form-control form-control-lg bg-light fs-6', 'placeholder':_('Nachname')}))
     
     class Meta:
         model = User
@@ -20,13 +21,13 @@ class RegisterUserForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
 
-        self.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control form-control-lg bg-light fs-6', 'placeholder': 'Username'})
+        self.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control form-control-lg bg-light fs-6', 'placeholder': _('Benutzername')})
         self.fields['username'].label = ""
 
-        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control form-control-lg bg-light fs-6', 'placeholder': 'Password'})
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control form-control-lg bg-light fs-6', 'placeholder': _('Passwort')})
         self.fields['password1'].label = ""
 
-        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control form-control-lg bg-light fs-6', 'placeholder': 'Confirm Password'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control form-control-lg bg-light fs-6', 'placeholder': _('Passwort bestätigen')})
         self.fields['password2'].label = ""
         
         for fieldname in ['username','password1','password2']:
