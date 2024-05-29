bfe191d88195d513a5d91ce9254a8dc1fe157a4f
Ben <benwiessner.dev@gmail.com>
2024-05-29 11:50:27 +0200

    lagerort update fix

--- static/js/delete_function.js
+++ static/js/delete_function.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     deleteButtons.forEach(function(deleteButton) {
         deleteButton.addEventListener('click', function() {
-            var form = deleteButton.closest('form');
+            var formId = deleteButton.getAttribute('data-tab');
+            var form = document.querySelector('#' + formId + ' form');
             if (form) {
                 if (confirm("Are you sure you want to delete the selected explantate(s)?")) {
                     form.submit();
--- staticfiles/js/checkbox_table.js
+++ staticfiles/js/checkbox_table.js
@@ -1,7 +1,10 @@
-document.getElementById('selectAll').addEventListener('change', function() {
-    var className = document.querySelector('#explant-table').classList.contains('huefte') ? 'huefte-row-checkbox' : 'knie-row-checkbox';
-    var checkboxes = document.querySelectorAll('.' + className);
-    for (var i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].checked = this.checked;
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    var selectAllCheckbox = document.querySelector('.selectAll');
+    selectAllCheckbox.addEventListener('change', function() {
+        var className = document.querySelector('#explant-table').classList.contains('huefte') ? 'huefte-row-checkbox' : 'knie-row-checkbox';
+        var checkboxes = document.querySelectorAll('.' + className);
+        for (var i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].checked = this.checked;
+        }
+    });
 });
